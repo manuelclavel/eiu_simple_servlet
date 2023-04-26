f6e330eec4ca2020210bf1ed1e452ad00b8737ac
EC2 Default User <ec2-user@ip-172-31-20-180.ap-southeast-1.compute.internal>
2023-04-26 02:41:49 +0000

    First commit

---
+++ src/main/webapp/js/scripts.js
@@ -0,0 +1,7 @@
+// JavaScript File
+const login_button = document.getElementById("login");
+login_button.addEventListener("click", loginAccount);
+
+function loginAccount() {
+  alert("Clicked!");
+}
